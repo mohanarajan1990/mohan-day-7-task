c84f88e77ddf8a7ec2203dd546fb520a20ef175c
mohanarajan1990 <99423268+mohanarajan1990@users.noreply.github.com>
2022-03-03 17:39:56 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,103 @@
+class object
+class student {
+    printName(name) {
+        console.log("this is your name ",name);
+    }
+}
+const obj = new student();
+obj.printName("mohan");
+constructor;
+class student {
+    printName(name) {
+        console.log("this is your name ",name);
+    }
+}
+const obj = new student();
+obj.printName("mohan");
+class student {
+        printName(name) {
+            console.log("this is your name ",name);
+        }
+    }
+    const obj = new student();
+    obj.printName("mohan");
+    constructor;
+    class student {
+        printName(name) {
+            console.log("this is your name ",name);
+        }
+    }
+    const obj = new student();
+    obj.printName("mohan");
+    class student {
+        constructor(name) {
+            this.name = name;
+            console.log("from mohan");
+            console.log(name);
+        }
+
+        printName() {
+            console.log("this is yourname ", this.name);
+            this.name = "devesh";
+            console.log(name);
+        }
+    }
+
+
+inheritance;
+class vehicle {
+    getEngine() {
+        console.log("mohan driving car");
+    }
+}
+class car extends vehicle {}
+
+let o = new car();
+o.getEngine()
+
+class vehicle {
+    getEngine() {
+        console.log("mohan driving car");
+    }
+}
+class car extends vehicle {
+    getEngine() {
+        console.log("mohan car is maruti");
+    }
+}
+
+let o = new car();
+o.getEngine()
+CLASS OVERLOADING;
+class overloading {
+    getEngine() {
+        console.log("overloading ",no);
+    }
+    getEngine(no) {
+        console.log("overloading 1");
+    }
+}
+let o = new overloading();
+o.getEngine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
